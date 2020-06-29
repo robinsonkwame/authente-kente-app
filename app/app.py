e09e3fdc2950279321759c6ed3c5fc80c1820bd7
sansitha nandakumar <46688505+sansi95@users.noreply.github.com>
2020-06-29 11:04:52 -0400

    (feat) flask
    (feat) Adds Flask, endpoint(s),
     
    * initialize lfs and track pickle
    * track .pkl as git lfs
    * First working version of flask app
    * Updated pickle files
    * Updating .pkl files again since they were over written by previous files from master
    * Added training option

--- app/app.py
+++ app/app.py
@@ -1,19 +1,41 @@
 from flask import *
 from feature import image_pros
+import os
+from make_dataset import makeinterim, makeprocessed
+from extract_features import extract_features
+from train_pred import train_pred
+
 app = Flask(__name__)
 
 # referred from https://www.javatpoint.com/flask-file-uploading
 @app.route('/')
 def upload():
-    return render_template("file_upload_form.html")
+    return render_template("options.html")
+
+# @app.route('/location')
+# def location():
+#
+#     return render_template('location.html')
 
 @app.route('/success', methods = ['POST'])
 def success():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(f.filename)
+        f.save('image.jpg')
     output = image_pros()
     return(output)
+
+@app.route('/train', methods = ['POST'])
+def train():
+    if request.method == 'POST':
+        f = request.files.getlist("my_file[]")
+        for i in f :
+            i.save('raw/'+i.filename)
+    makeinterim()
+    makeprocessed()
+    extract_features ()
+    table = train_pred()
+    return (table)
 
 if __name__ == '__main__':
     app.run(debug = True)
